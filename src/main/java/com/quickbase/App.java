4eb03a7c4d604b5f12be447084404e3d8166d515
Misho <michaelbozhilov@gmail.com>
2022-04-11 20:51:23 +0300

    Finishing touches

--- src/main/java/com/quickbase/App.java
+++ src/main/java/com/quickbase/App.java
@@ -6,6 +6,8 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -20,6 +22,7 @@ public class App {
     private static final String FRESHDESK_API = ".freshdesk.com/api/v2/contacts";
 
     public static Map<String, String> getGithubInfo(String GITHUB_TOKEN) throws Exception {
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .GET()
@@ -28,16 +31,18 @@ public class App {
                 .uri(URI.create(GITHUB_API))
                 .timeout(Duration.ofSeconds(5)) // timeout request after 5 seconds
                 .build();
+
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()); // send the request synchronously
         int response_code = response.statusCode();
-        if(response_code>299)throw new Exception("Invalid token"); // throw exception if sent information is incorrect
+        if(response_code>500) throw new Exception("Unable to reach GITHUB Servers");
+        else if(response_code>299)throw new Exception("Invalid token"); // throw exception if sent information is incorrect
 
 
         JSONObject jsonObject = (JSONObject) new JSONParser().parse(response.body()); // parses response as JSON Object
 
         Map<String, String> dictionary = new HashMap<String, String>();
 
-        // Map received values so we can exclude null values afterwards when doing a post request
+        // Map received values to corresponding names and check for null values
 
         buildMapElements(jsonObject, "name", "name", dictionary); // name of user in github
         buildMapElements(jsonObject, "email", "email", dictionary); // email address (unique value in freshdesk)
@@ -46,7 +51,6 @@ public class App {
         buildMapElements(jsonObject, "address", "location", dictionary);
         buildMapElements(jsonObject, "company_id", "company", dictionary);
 
-        if(dictionary == null) throw new Exception("No response data provided"); // the possibility of fulfilling this check is only if Freshdesk change their api
         return dictionary;
 
     }
@@ -58,34 +62,32 @@ public class App {
 
     public static void postFreshdeskInfo(String FRESHDESK_TOKEN, Map<String, String> dictionary) throws Exception {
 
-
         StringBuffer http_encode = new StringBuffer("https://"); // get subdomain
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please, insert Freshdesk subdomain:");
         StringBuffer subdomain = new StringBuffer(scanner.nextLine());
-        if(subdomain.isEmpty()) throw new Exception("Empty subdomain");
         scanner.close();
+        if(subdomain.isEmpty()) throw new Exception("Empty subdomain");
 
         // using StringBuffer because they are more efficient when it comes to concatenation
 
-        StringBuffer uri_put_req = new StringBuffer();
-        uri_put_req.append(http_encode).append(subdomain).append(FRESHDESK_API);
+        StringBuffer uri_req_address = new StringBuffer();
+        uri_req_address.append(http_encode).append(subdomain).append(FRESHDESK_API);
 
-        JSONObject jsonObject = new JSONObject(dictionary); //create a json object from map -- incoming_info
+        JSONObject incoming_info_json = new JSONObject(dictionary); //create a json object from map -- incoming_info
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .POST(HttpRequest.BodyPublishers.ofString(jsonObject.toJSONString())) // pass the object in the body of request
+                .POST(HttpRequest.BodyPublishers.ofString(incoming_info_json.toJSONString())) // pass the object in the body of request
                 .header("Content-Type", "application/json")
                 .header("Authorization", "Basic " + Base64.getEncoder().encodeToString((FRESHDESK_TOKEN).getBytes()))
-                .uri(URI.create(uri_put_req.toString()))
+                .uri(URI.create(uri_req_address.toString()))
                 .timeout(Duration.ofSeconds(5))
                 .build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()); // send the request synchronously
         int response_code = response.statusCode();
 
-        System.out.println("RESPONSE on post: " + response_code);
         if(response_code < 300) System.out.println("Contact added successfully!");
         else if(response_code == 400) throw new Exception("At least an email, phone number or twitter id should be provided");
         else if(response_code == 404) throw new Exception("Subdomain not found");
@@ -102,27 +104,36 @@ public class App {
             String id = addInfoObj.get("user_id").toString();
 
             HttpRequest put_request = HttpRequest.newBuilder()
-                    .PUT(HttpRequest.BodyPublishers.ofString(jsonObject.toJSONString()))
+                    .PUT(HttpRequest.BodyPublishers.ofString(incoming_info_json.toJSONString()))
                     .header("Content-Type", "application/json")
                     .header("Authorization", "Basic " + Base64.getEncoder().encodeToString((FRESHDESK_TOKEN).getBytes()))
-                    .uri(URI.create(uri_put_req.append("/").append(id).toString()))
+                    .uri(URI.create(uri_req_address.append("/").append(id).toString()))
                     .timeout(Duration.ofSeconds(5))
                     .build();
 
             response = client.send(put_request, HttpResponse.BodyHandlers.ofString());
             response_code = response.statusCode();
 
-            System.out.println("RESPONSE on put:" + response_code);
             if(response_code < 300) System.out.println("Contact updated"); else throw new Exception("Could not update contact");
         } else throw new Exception("Unexpected error");
     }
 
+    public static void checkInternetConnect(URL url) {
+        // Could be written with a custom exception
+        try {
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.connect();
+        } catch (Exception e){
+            System.out.println("No Internet connection");
+            System.exit(0);
+        }
+    }
 
 
     public static void main(String[] args) {
         Map<String, String> dictionary;
-
         try{
+            checkInternetConnect(new URL(GITHUB_API));
             dictionary = getGithubInfo(GITHUB_TOKEN);
             postFreshdeskInfo(FRESHDESK_TOKEN, dictionary);
         } catch (Exception e){
